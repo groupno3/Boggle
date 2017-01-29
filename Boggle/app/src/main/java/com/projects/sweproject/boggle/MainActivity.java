a21dbd60971ebd78737ea7537bfab69d65a9419c
MP <emenpy@Minhs-MacBook-Pro.local>
2017-01-29 15:01:58 -0800

    First commit

--- Boggle/app/src/main/java/com/projects/sweproject/boggle/MainActivity.java
+++ Boggle/app/src/main/java/com/projects/sweproject/boggle/MainActivity.java
@@ -1,13 +1,29 @@
 package com.projects.sweproject.boggle;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+
+        actionBar.hide();
+
+
+
     }
 }
