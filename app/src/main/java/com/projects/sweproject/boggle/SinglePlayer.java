109d0fbe89f49f787720db3b0ba3ffac21a145b5
akaminnya <minphan@pdx.edu>
2017-01-30 18:01:48 -0800

    Implemented a sample 3 minute timer

--- app/src/main/java/com/projects/sweproject/boggle/SinglePlayer.java
+++ app/src/main/java/com/projects/sweproject/boggle/SinglePlayer.java
@@ -2,11 +2,17 @@ package com.projects.sweproject.boggle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.util.*;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by emenpy on 1/30/17.
  */
 public class SinglePlayer extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,23 @@ public class SinglePlayer extends Activity {
 
         setContentView(R.layout.single_player);
 
+        final TextView a = (TextView) findViewById(R.id.timer);
+
+
+        new CountDownTimer(180000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+
+
+
+                a.setText("Time remaining: " + ((millisUntilFinished/1000)/60)  + " minute and "+ ((millisUntilFinished/1000)%60) + " seconds");
+            }
+
+            public void onFinish() {
+                a.setText("done!");
+            }
+        }.start();
 
 
     }
