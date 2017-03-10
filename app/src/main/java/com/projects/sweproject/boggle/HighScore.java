d54d47035589f8509a6ce0ee6a98b825115662bb
minh phan <minh phan>
2017-03-10 00:42:57 -0800

    Added HighScore for Multi player

--- app/src/main/java/com/projects/sweproject/boggle/HighScore.java
+++ app/src/main/java/com/projects/sweproject/boggle/HighScore.java
@@ -1,7 +1,11 @@
 package com.projects.sweproject.boggle;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,42 +16,33 @@ import org.w3c.dom.Text;
 
 public class HighScore extends Activity {
 
-
-    TextView easyHighScoresNames;
-    TextView easyHighScores;
-    TextView mediumHighScoresNames;
-    TextView mediumHighScores;
-    TextView hardHighScoresNames;
-    TextView hardHighScores;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.highscore_screen);
+
+        setContentView(R.layout.highscore);
+
+        ImageButton single = (ImageButton) findViewById(R.id.singlebtn);
+        single.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(HighScore.this, HighScoreSingle.class);
+                startActivity(intent);
 
 
-        HighScoreDBHelper scoreDBHelper = new HighScoreDBHelper(getApplicationContext());
-        HighScoreKeeper keeper = scoreDBHelper.getTopFiveScoreboard();
+            }
+        });
 
 
-
-        easyHighScoresNames = (TextView) findViewById(R.id.easyNames);
-        easyHighScores = (TextView) findViewById(R.id.easyScores);
-
-        easyHighScoresNames.setText(keeper.displayEasyNames());
-        easyHighScores.setText(keeper.displayEasyScores());
-
-        mediumHighScoresNames = (TextView) findViewById(R.id.mediumNames);
-        mediumHighScores = (TextView) findViewById(R.id.mediumScores);
-
-        mediumHighScoresNames.setText(keeper.displayMediumNames());
-        mediumHighScores.setText(keeper.displayMediumScores());
-
-        hardHighScoresNames = (TextView) findViewById(R.id.hardNames);
-        hardHighScores = (TextView) findViewById(R.id.hardScores);
-
-        hardHighScoresNames.setText(keeper.displayHardNames());
-        hardHighScores.setText(keeper.displayHardScores());
+        ImageButton multi = (ImageButton) findViewById(R.id.multibtn);
+        multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HighScore.this, HighScoreMulti.class);
+                startActivity(intent);
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/projects/sweproject/boggle/HighScoreMulti.java
@@ -0,0 +1,52 @@
+package com.projects.sweproject.boggle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by emenp on 3/10/2017.
+ */
+
+public class HighScoreMulti extends Activity {
+
+    TextView easyHighScoresNames;
+    TextView easyHighScores;
+    TextView mediumHighScoresNames;
+    TextView mediumHighScores;
+    TextView hardHighScoresNames;
+    TextView hardHighScores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscore_single);
+
+
+        HighScoreMultiPlayerDBHelper scoreDBHelper = new HighScoreMultiPlayerDBHelper(getApplicationContext());
+        HighScoreKeeper keeper = scoreDBHelper.getTopFiveScoreboard();
+
+
+
+        easyHighScoresNames = (TextView) findViewById(R.id.easyNames);
+        easyHighScores = (TextView) findViewById(R.id.easyScores);
+
+        easyHighScoresNames.setText(keeper.displayEasyNames());
+        easyHighScores.setText(keeper.displayEasyScores());
+
+        mediumHighScoresNames = (TextView) findViewById(R.id.mediumNames);
+        mediumHighScores = (TextView) findViewById(R.id.mediumScores);
+
+        mediumHighScoresNames.setText(keeper.displayMediumNames());
+        mediumHighScores.setText(keeper.displayMediumScores());
+
+        hardHighScoresNames = (TextView) findViewById(R.id.hardNames);
+        hardHighScores = (TextView) findViewById(R.id.hardScores);
+
+        hardHighScoresNames.setText(keeper.displayHardNames());
+        hardHighScores.setText(keeper.displayHardScores());
+
+
+    }
+
+}
